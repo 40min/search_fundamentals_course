1f9f926441b91c164b9469bb432696571a040635
Andrei Sorokin <andrei.sorokin@wolt.com>
2023-02-11 16:48:55 +0200

    Rollback changes for local

--- count-tracker.sh
+++ count-tracker.sh
@@ -4,6 +4,6 @@ do
   echo "Queries:"
   curl -k -XGET -u admin:admin  "https://localhost:9200/_cat/count/bbuy_queries";
   echo "Products:"
-  curl -k -XGET -u admin:admin  "http://localhost:9200/_cat/count/bbuy_products";
+  curl -k -XGET -u admin:admin  "https://localhost:9200/_cat/count/bbuy_products";
   sleep 100;
 done
